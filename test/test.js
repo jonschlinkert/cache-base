0bde6350e3de9788b77b3cf281445e14dbca5145
jonschlinkert <jon.schlinkert@sellside.com>
2015-11-23 22:37:00 -0500

    lint

--- test/test.js
+++ test/test.js
@@ -13,18 +13,18 @@ var Cache;
 var cache;
 var app;
 
-describe('cache-base', function () {
+describe('cache-base', function() {
   beforeEach(function() {
     Cache = require('..');
     app = new Cache();
   });
 
-  describe('constructor:', function () {
-    it('should create an instance of Cache', function () {
+  describe('constructor:', function() {
+    it('should create an instance of Cache', function() {
       assert(app instanceof Cache);
     });
 
-    it('should set ', function () {
+    it('should set ', function() {
       var app = new Cache({
         one: 1,
         two: 2
@@ -34,7 +34,7 @@ describe('cache-base', function () {
     });
   });
 
-  describe('get/set:', function () {
+  describe('get/set:', function() {
     beforeEach(function() {
       Cache = require('..');
       app = new Cache();
@@ -44,49 +44,49 @@ describe('cache-base', function () {
       app.clear();
     });
 
-    describe('set() - add:', function () {
-      it('should set a new property with the given value', function () {
+    describe('set() - add:', function() {
+      it('should set a new property with the given value', function() {
         app.set('one', 1);
         assert.equal(app.get('one'), 1);
       });
     });
 
-    describe('set() - update:', function () {
-      it('should update an existing property with the given value', function () {
+    describe('set() - update:', function() {
+      it('should update an existing property with the given value', function() {
         app.set('one', 2);
         assert.equal(app.get('one'), 2);
       });
 
-      it('should get the given property', function () {
+      it('should get the given property', function() {
         app.set('a', 'b');
         assert.equal(app.get('a'), 'b');
       });
     });
   });
 
-  describe('.set()', function () {
-    it('should set a value', function () {
+  describe('.set()', function() {
+    it('should set a value', function() {
       app.set('a', 'b');
       assert.equal(app.get('a'), 'b');
     });
 
-    it('should set properties on the `cache` object.', function () {
+    it('should set properties on the `cache` object.', function() {
       app.set('a', 'b');
       assert.equal(app.cache.a, 'b');
     });
 
-    it('should allow an object to be set directly.', function () {
+    it('should allow an object to be set directly.', function() {
       app.set({x: 'y'});
       assert.equal(app.cache.x, 'y');
       assert.equal(app.get('x'), 'y');
     });
 
-    it('should set nested properties on the `cache` object.', function () {
+    it('should set nested properties on the `cache` object.', function() {
       app.set('c', {d: 'e'});
       assert.equal(app.get('c').d, 'e');
     });
 
-    it('should return `this` for chaining', function () {
+    it('should return `this` for chaining', function() {
       assert.equal(app.set('a', 'b'), app);
       app
         .set('aa', 'bb')
@@ -97,36 +97,59 @@ describe('cache-base', function () {
       assert.equal(app.get('cc'), 'dd');
     });
 
-    it('should return undefined when not set', function () {
+    it('should return undefined when not set', function() {
       assert.equal(app.set('a', undefined), app);
     });
   });
 
-  describe('.get()', function () {
-    it('should return undefined when no set', function () {
+  describe('.get()', function() {
+    it('should return undefined when no set', function() {
       assert(app.get('a') === undefined);
     });
 
-    it('should otherwise return the value', function () {
+    it('should otherwise return the value', function() {
       app.set('a', 'b');
       assert.equal(app.get('a'), 'b');
     });
   });
 });
 
-describe('namespace', function () {
+describe('events', function() {
   beforeEach(function() {
     cache = require('..');
     Cache = cache.namespace('data');
     app = new Cache();
   });
 
-  describe('constructor:', function () {
-    it('should create an instance of Cache', function () {
+  it('should emit a set event', function() {
+
+  });
+});
+
+
+describe('namespace events', function() {
+  beforeEach(function() {
+    cache = require('..');
+    Cache = cache.namespace('data');
+    app = new Cache();
+  });
+
+
+});
+
+describe('namespace', function() {
+  beforeEach(function() {
+    cache = require('..');
+    Cache = cache.namespace('data');
+    app = new Cache();
+  });
+
+  describe('constructor:', function() {
+    it('should create an instance of Cache', function() {
       assert(app instanceof Cache);
     });
 
-    it('should set ', function () {
+    it('should set ', function() {
       var app = new Cache({
         one: 1,
         two: 2
@@ -136,50 +159,50 @@ describe('namespace', function () {
     });
   });
 
-  describe('get/set:', function () {
-    describe('set() - add:', function () {
-      it('should set a new property with the given value', function () {
+  describe('get/set:', function() {
+    describe('set() - add:', function() {
+      it('should set a new property with the given value', function() {
         app.set('one', 1);
         assert.equal(app.get('one'), 1);
       });
     });
 
-    describe('set() - update:', function () {
-      it('should update an existing property with the given value', function () {
+    describe('set() - update:', function() {
+      it('should update an existing property with the given value', function() {
         app.set('one', 2);
         assert.equal(app.get('one'), 2);
       });
 
-      it('should get the given property', function () {
+      it('should get the given property', function() {
         app.set('a', 'b');
         assert.equal(app.get('a'), 'b');
       });
     });
   });
 
-  describe('.set()', function () {
-    it('should set a value', function () {
+  describe('.set()', function() {
+    it('should set a value', function() {
       app.set('a', 'b');
       assert.equal(app.get('a'), 'b');
     });
 
-    it('should set properties on the `cache` object.', function () {
+    it('should set properties on the `cache` object.', function() {
       app.set('a', 'b');
       assert.equal(app.data.a, 'b');
     });
 
-    it('should allow an object to be set directly.', function () {
+    it('should allow an object to be set directly.', function() {
       app.set({x: 'y'});
       assert.equal(app.data.x, 'y');
       assert.equal(app.get('x'), 'y');
     });
 
-    it('should set nested properties on the `cache` object.', function () {
+    it('should set nested properties on the `cache` object.', function() {
       app.set('c', {d: 'e'});
       assert.equal(app.get('c').d, 'e');
     });
 
-    it('should return `this` for chaining', function () {
+    it('should return `this` for chaining', function() {
       assert.equal(app.set('a', 'b'), app);
       app
         .set('aa', 'bb')
@@ -190,17 +213,17 @@ describe('namespace', function () {
       assert.equal(app.get('cc'), 'dd');
     });
 
-    it('should return undefined when not set', function () {
+    it('should return undefined when not set', function() {
       assert.equal(app.set('a', undefined), app);
     });
   });
 
-  describe('.get()', function () {
-    it('should return undefined when no set', function () {
+  describe('.get()', function() {
+    it('should return undefined when no set', function() {
       assert(app.get('a') === undefined);
     });
 
-    it('should otherwise return the value', function () {
+    it('should otherwise return the value', function() {
       app.set('a', 'b');
       assert.equal(app.get('a'), 'b');
     });
