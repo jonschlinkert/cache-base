3612667524873012061b2bddb72d09f7fdd44b47
jonschlinkert <jon.schlinkert@sellside.com>
2015-11-23 22:31:42 -0500

    refactor, simplify

--- index.js
+++ index.js
@@ -1,359 +1,165 @@
 'use strict';
 
-var util = require('util');
-var typeOf = require('kind-of');
-var Options = require('option-cache');
+var isObject = require('isobject');
+var visit = require('collection-visit');
 var get = require('get-value');
-var _ = require('lodash');
+var has = require('has-value');
+var set = require('set-value');
+var unset = require('unset-value');
+
+/**
+ * Create a `Cache` constructor that, when instantiated, will
+ * store values on the given `prop`.
+ *
+ * ```js
+ * var Cache = require('cache-base').namespace('data');
+ * var cache = new Cache();
+ * ```
+ * @param {String} `prop` The property name to use for storing values.
+ * @return {Function} Returns a custom `Cache` constructor
+ * @api public
+ */
+
+function namespace(prop) {
+  prop = prop || 'cache';
+
+  /**
+   * Create a new `Cache`. Internally the `Cache` constructor is created using
+   * the `namespace` function, with `cache` defined as the storage object.
+   *
+   * ```js
+   * var app = new Cache();
+   * ```
+   * @param {Object} `cache` Optionally pass an object to initialize with.
+   * @constructor
+   * @api public
+   */
+
+  function Cache(cache) {
+    this[prop] = {};
+    if (cache) {
+      this.set(cache);
+    }
+  }
+
+  /**
+   * Set property `key` with the given `value`.
+   *
+   * ```js
+   * app.set('a', 'b');
+   * // or
+   * app.set({a: 'b'});
+   * ```
+   *
+   * @param {String} `key`
+   * @param {any} `value`
+   * @return {Cache} Returns the instance for chaining
+   * @api public
+   */
+
+  Cache.prototype.set = function(key, value) {
+    if (isObject(key)) {
+      return this.visit('set', key);
+    }
+    set(this[prop], key, value);
+    return this;
+  };
+
+  /**
+   * Return the stored value of `key`. If `key` is not defined,
+   * the `cache` is returned.
+   *
+   * ```js
+   * app.set('foo', 'bar');
+   * app.get('foo');
+   * //=> "bar"
+   * ```
+   *
+   * @param {String} `key`
+   * @api public
+   */
+
+  Cache.prototype.get = function(key) {
+    return get(this[prop], key);
+  };
+
+  /**
+   * Return true if cache `key` is not undefined or null.
+   *
+   * ```js
+   * app.set('foo', 'bar');
+   * app.has('foo');
+   * //=> true
+   * ```
+   *
+   * @param {String} `key`
+   * @api public
+   */
+
+  Cache.prototype.has = function(key) {
+    return has(this[prop], key);
+  };
+
+  /**
+   * Delete one or more properties from the cache.
+   *
+   * ```js
+   * app.del('foo');
+   * // or
+   * app.del(['foo', 'bar']);
+   * ```
+   * @param {String|Array} `keys`
+   * @api public
+   */
+
+  Cache.prototype.del = function(key) {
+    if (Array.isArray(key)) {
+      return this.visit('del', key);
+    }
+    unset(this[prop], key);
+    return this;
+  };
+
+  /**
+   * Reset the entire cache to an empty object.
+   *
+   * ```js
+   * app.clear();
+   * ```
+   * @api public
+   */
+
+  Cache.prototype.clear = function() {
+    this[prop] = {};
+  };
+
+  /**
+   * Visit `method`, or map-visit `method`, over each property in `val`.
+   *
+   * ```js
+   * app.visit('set', {a: 'b'});
+   * ```
+   * @param {String} `method` The name of the method to call.
+   * @param {Object|Array} `val` The object or array to iterate over.
+   * @api public
+   */
+
+  Cache.prototype.visit = function(method, val) {
+    visit(this, method, val);
+    return this;
+  };
+
+  return Cache;
+}
 
 /**
  * Expose `Cache`
  */
 
-module.exports = Cache;
+module.exports = namespace('cache');
 
 /**
- * Create a new instance of `Cache`
- *
- * ```js
- * var app = new Cache();
- * ```
- *
- * @class Cache
- * @param {Object} `cache` Optionally pass an object to initialize with.
- * @constructor
- * @api public
+ * Expose `Cache.namespace`
  */
 
-function Cache(cache) {
-  Options.call(this);
-  this.cache = cache || {};
-}
-
-util.inherits(Cache, Options);
-
-/**
- * Assign `value` to `key` or return the value of `key`.
- *
- * ```js
- * app.set(key, value);
- *
- * // extend
- * app.set({a: 'b'});
- * ```
- *
- * @param {String} `key`
- * @param {*} `value`
- * @return {Cache} for chaining
- * @api public
- */
-
-Cache.prototype.set = function (key, value) {
-  if (typeOf(key) === 'object') {
-    _.extend(this.cache, key);
-  } else {
-    this.cache[key] = value;
-  }
-  return this;
-};
-
-/**
- * Return the stored value of `key`. If `key` is not defined,
- * the `cache` is returned.
- *
- * ```js
- * app.set('foo', 'bar');
- * app.get('foo');
- * // => "bar"
- * ```
- *
- * @param {String} `key`
- * @api public
- */
-
-Cache.prototype.get = function (key) {
-  if (!key) return this.clone();
-
-  if (key.indexOf('.') !== -1) {
-    return get(this.cache, key, true);
-  }
-  return this.cache[key];
-};
-
-/*
- * Return `true` if the element exists. Dot notation
- * may be used for nested properties.
- *
- * ```js
- * app.exists('author.name');
- * //=> true
- * ```
- *
- * @param {String} `key`
- * @return {Boolean}
- * @api public
- */
-
-Cache.prototype.exists = function(key) {
-  if (this.hasOwn(key)) {
-    return true;
-  }
-  return get(this.cache, key, true) !== undefined;
-};
-
-/**
- * Extend the `cache` with the given object.
- *
- * ```js
- * app
- *   .extend({a: 'b'}, {c: 'd'});
- *   .extend('e', {f: 'g'});
- * ```
- *
- * @chainable
- * @return {Object} `Cache` to enable chaining.
- * @api public
- */
-
-Cache.prototype.extend = function(val) {
-  var args = [].concat.apply([], arguments);
-
-  if (typeof val === 'string') {
-    var rest = args.slice(1);
-    var o = this.get(val) || {};
-    o = extend(o, rest);
-    this.cache[val] = o;
-  } else {
-    extend(this.cache, args);
-  }
-
-  return this;
-};
-
-/**
- * Deep merge an object onto the `cache`.
- *
- * ```js
- * app.merge({a: {one: 'one'}}, {a: {two: 'two'}});
- * console.log(app.get('a'));
- * //=> {a: {one: 'one', two: 'two'}}
- * ```
- *
- * @chainable
- * @return {Object} `Cache` to enable chaining.
- * @api public
- */
-
-Cache.prototype.merge = function(val) {
-  var args = [].concat.apply([], arguments);
-
-  if (typeof val === 'string') {
-    var rest = args.slice(1);
-    var o = this.get(val) || {};
-    o = merge(o, rest);
-    this.cache[val] = o;
-  } else {
-    merge(this.cache, args);
-  }
-
-  return this;
-};
-
-/**
- * Extend the cache with only the specified values from the given
- * object.
- *
- * ```js
- * var obj = {a: 'a', b: 'b', c: 'c'};
- * app.pick(obj, 'a');
- * //=> {a: 'a'}
- *
- * app.pick(obj, ['a', 'b']);
- * //=> {a: 'a', b: 'b'}
- * ```
- *
- * @param {String|Array} `key` The key(s) to pick from the object and extend onto `app.cache`
- * @api public
- */
-
-Cache.prototype.pick = function(o, keys) {
-  this.extend(_.pick(o, keys));
-};
-
-/**
- * Omit a property or array of properties from the cache
- *
- * ```js
- * app.omit('foo');
- * // or
- * app.omit(['foo', 'bar']);
- * ```
- *
- * @param {String|Array} `key` The key(s) to omit from the cache
- * @api public
- */
-
-Cache.prototype.omit = function(keys) {
-  this.cache = _.omit(this.cache, keys);
-};
-
-/**
- * Return the keys on `obj` or `this.cache`.
- *
- * ```js
- * app.forOwn();
- * ```
- *
- * @param {Object} `obj` Optionally pass an object.
- * @return {Array} Array of keys.
- * @api public
- */
-
-Cache.prototype.forOwn = function (o, fn, thisArg) {
-  if (typeof o === 'function') {
-    thisArg = fn; fn = o; o = this.cache;
-  }
-  return _.forOwn(o, fn, thisArg || this);
-};
-
-/**
- * Return the keys on `obj` or `this.cache`.
- *
- *
- * ```js
- * app.keys();
- * ```
- *
- * @param {Object} `obj` Optionally pass an object.
- * @return {Array} Array of keys.
- * @api public
- */
-
-Cache.prototype.keys = function (o) {
-  return Object.keys(o || this.cache);
-};
-
-/**
- * Return an object of only the properties on `this.cache` or the given `obj`
- * that have function values.
- *
- * ```js
- * app.functions('foo')
- * //=> {set: [function], get: [function], functions: [function]}
- * ```
- *
- * @param {Object} `obj`
- * @return {Array}
- * @api public
- */
-
-Cache.prototype.functions = function(o) {
-  o = o || this.cache;
-  var fns = [];
-
-  _.forIn(o, function (val, key) {
-    if (typeof val === 'function') {
-      fns[key] = val;
-    }
-  });
-
-  return fns;
-};
-
-/**
- * Return true if a deep property is on the given object or
- * `this.cache`.
- *
- * ```js
- * app.has('a.b.c');
- * ```
- *
- * @param {Object} `obj` Optionally pass an object.
- * @return {String} `lookup` Prop string to use for the lookup, e.g. `a.b`
- * @api public
- */
-
-Cache.prototype.has = function (val, lookup) {
-  if (arguments.length === 1 && typeof val === 'string') {
-    return get(this.cache, val) !== undefined;
-  }
-  return get(val, lookup) !== undefined;
-};
-
-/**
- * Return true if `key` is an own, enumerable property
- * of `this.cache` or the given `obj`.
- *
- * ```js
- * app.hasOwn(key);
- * // or
- * app.hasOwn(obj, key);
- * ```
- *
- * @param  {String} `key`
- * @param  {Object} `obj` Optionally pass an object to check.
- * @return {Boolean}
- * @api public
- */
-
-Cache.prototype.hasOwn = function (o, key) {
-  if (typeof o === 'string') {
-    key = o; o = this.cache;
-  }
-  return {}.hasOwnProperty.call(o, key);
-};
-
-/**
- * Clone the given `obj` or `cache`.
- *
- * ```js
- * app.clone();
- * ```
- *
- * @param  {Object} `obj` Optionally pass an object to clone.
- * @return {Boolean}
- * @api public
- */
-
-Cache.prototype.clone = function(o) {
-  return _.cloneDeep(o || this.cache);
-};
-
-/**
- * Remove `key` from the cache, or if no value is
- * specified the entire cache is reset.
- *
- * **Example:**
- *
- * ```js
- * app.clear();
- * ```
- *
- * @param {String} `key` The property to remove.
- * @api public
- */
-
-Cache.prototype.clear = function (key) {
-  if (key) {
-    delete this.cache[key];
-  } else {
-    this.cache = {};
-  }
-};
-
-
-/**
- * Utils
- */
-
-function extend(o, objects) {
-  return _.extend.apply(_, [o].concat(objects));
-}
-
-function merge(o, objects) {
-  return _.merge.apply(_, [o].concat(objects));
-}
-
-function union () {
-  return [].concat.apply([], arguments);
-}
+module.exports.namespace = namespace;
---
+++ test/test.js
@@ -0,0 +1,208 @@
+/*!
+ * cache-base <https://github.com/jonschlinkert/cache-base>
+ *
+ * Copyright (c) 2014-2015, Jon Schlinkert.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+require('mocha');
+var assert = require('assert');
+var Cache;
+var cache;
+var app;
+
+describe('cache-base', function () {
+  beforeEach(function() {
+    Cache = require('..');
+    app = new Cache();
+  });
+
+  describe('constructor:', function () {
+    it('should create an instance of Cache', function () {
+      assert(app instanceof Cache);
+    });
+
+    it('should set ', function () {
+      var app = new Cache({
+        one: 1,
+        two: 2
+      });
+      assert.equal(app.cache.one, 1);
+      assert.equal(app.cache.two, 2);
+    });
+  });
+
+  describe('get/set:', function () {
+    beforeEach(function() {
+      Cache = require('..');
+      app = new Cache();
+    });
+
+    afterEach(function() {
+      app.clear();
+    });
+
+    describe('set() - add:', function () {
+      it('should set a new property with the given value', function () {
+        app.set('one', 1);
+        assert.equal(app.get('one'), 1);
+      });
+    });
+
+    describe('set() - update:', function () {
+      it('should update an existing property with the given value', function () {
+        app.set('one', 2);
+        assert.equal(app.get('one'), 2);
+      });
+
+      it('should get the given property', function () {
+        app.set('a', 'b');
+        assert.equal(app.get('a'), 'b');
+      });
+    });
+  });
+
+  describe('.set()', function () {
+    it('should set a value', function () {
+      app.set('a', 'b');
+      assert.equal(app.get('a'), 'b');
+    });
+
+    it('should set properties on the `cache` object.', function () {
+      app.set('a', 'b');
+      assert.equal(app.cache.a, 'b');
+    });
+
+    it('should allow an object to be set directly.', function () {
+      app.set({x: 'y'});
+      assert.equal(app.cache.x, 'y');
+      assert.equal(app.get('x'), 'y');
+    });
+
+    it('should set nested properties on the `cache` object.', function () {
+      app.set('c', {d: 'e'});
+      assert.equal(app.get('c').d, 'e');
+    });
+
+    it('should return `this` for chaining', function () {
+      assert.equal(app.set('a', 'b'), app);
+      app
+        .set('aa', 'bb')
+        .set('bb', 'cc')
+        .set('cc', 'dd');
+      assert.equal(app.get('aa'), 'bb');
+      assert.equal(app.get('bb'), 'cc');
+      assert.equal(app.get('cc'), 'dd');
+    });
+
+    it('should return undefined when not set', function () {
+      assert.equal(app.set('a', undefined), app);
+    });
+  });
+
+  describe('.get()', function () {
+    it('should return undefined when no set', function () {
+      assert(app.get('a') === undefined);
+    });
+
+    it('should otherwise return the value', function () {
+      app.set('a', 'b');
+      assert.equal(app.get('a'), 'b');
+    });
+  });
+});
+
+describe('namespace', function () {
+  beforeEach(function() {
+    cache = require('..');
+    Cache = cache.namespace('data');
+    app = new Cache();
+  });
+
+  describe('constructor:', function () {
+    it('should create an instance of Cache', function () {
+      assert(app instanceof Cache);
+    });
+
+    it('should set ', function () {
+      var app = new Cache({
+        one: 1,
+        two: 2
+      });
+      assert.equal(app.data.one, 1);
+      assert.equal(app.data.two, 2);
+    });
+  });
+
+  describe('get/set:', function () {
+    describe('set() - add:', function () {
+      it('should set a new property with the given value', function () {
+        app.set('one', 1);
+        assert.equal(app.get('one'), 1);
+      });
+    });
+
+    describe('set() - update:', function () {
+      it('should update an existing property with the given value', function () {
+        app.set('one', 2);
+        assert.equal(app.get('one'), 2);
+      });
+
+      it('should get the given property', function () {
+        app.set('a', 'b');
+        assert.equal(app.get('a'), 'b');
+      });
+    });
+  });
+
+  describe('.set()', function () {
+    it('should set a value', function () {
+      app.set('a', 'b');
+      assert.equal(app.get('a'), 'b');
+    });
+
+    it('should set properties on the `cache` object.', function () {
+      app.set('a', 'b');
+      assert.equal(app.data.a, 'b');
+    });
+
+    it('should allow an object to be set directly.', function () {
+      app.set({x: 'y'});
+      assert.equal(app.data.x, 'y');
+      assert.equal(app.get('x'), 'y');
+    });
+
+    it('should set nested properties on the `cache` object.', function () {
+      app.set('c', {d: 'e'});
+      assert.equal(app.get('c').d, 'e');
+    });
+
+    it('should return `this` for chaining', function () {
+      assert.equal(app.set('a', 'b'), app);
+      app
+        .set('aa', 'bb')
+        .set('bb', 'cc')
+        .set('cc', 'dd');
+      assert.equal(app.get('aa'), 'bb');
+      assert.equal(app.get('bb'), 'cc');
+      assert.equal(app.get('cc'), 'dd');
+    });
+
+    it('should return undefined when not set', function () {
+      assert.equal(app.set('a', undefined), app);
+    });
+  });
+
+  describe('.get()', function () {
+    it('should return undefined when no set', function () {
+      assert(app.get('a') === undefined);
+    });
+
+    it('should otherwise return the value', function () {
+      app.set('a', 'b');
+      assert.equal(app.get('a'), 'b');
+    });
+  });
+});
